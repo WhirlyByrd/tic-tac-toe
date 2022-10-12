f9cee75437f0f0b6477fcce6d725512e92076398
WhirlyByrd <kara.r.gardner@gmail.com>
2022-10-12 12:19:06 -0500

    adding props to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Square from "./Square"
+//destructure useState from React
+import {useState} from 'react'
 
 function App() {
+  // Using array destructuring initialize the state for the games squares to an array of 9 empty strings
+  //setSquares name of function
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
+  
+  //initialize state of the player variable to a boolean of true
+  //setPlayer name of function
+  const [player, setPlayer] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* Add a prop of squares that is equal to the squares state variable to the Square component
+
+      Add a prop of setSquares that is equal to the setSquares function to the Square component
+
+      Add a prop of player that is equal to the player state variable to the Square component
+
+      Add a prop of setPlayer that is equal to the setPlayer function to the Square component */}
+      <Square 
+      squares={squares}
+      setSquares={setSquares}
+      player={player}
+      setPlayer={setPlayer}
+      />
     </div>
   );
 }
 
 export default App;
+ 
---
+++ src/Square.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Square = (props) => {
+
+    console.log(props.squares, props.player)
+   
+    // return (
+    //     <div className="square">
+    //     </div>
+    // )
+}
+
+export default Square;
